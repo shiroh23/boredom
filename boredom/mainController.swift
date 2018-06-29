5914e8fc66f2e1856cdb80e8aa070a2f2cee5286
Richárd Horváth <ricsy7@gmail.com>
2018-06-29 18:06:39 +0200

    shake funkció pipa

--- boredom/mainController.swift
+++ boredom/mainController.swift
@@ -25,9 +25,23 @@ class mainController: UIViewController {
 
 }
 
+//ezzel ismeri fel a dupla kattintást, és írja le hogy hogyan reagáljon rá
 extension mainController: UIGestureRecognizerDelegate {
     func handleTap(_ gesture: UITapGestureRecognizer){
-        print("doubletapped")
+        self.helloLbl.shake()
+    }
+}
+
+//ezzel shake animáció érhető el
+extension UILabel {
+    func shake(){
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 10, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 10, y: self.center.y))
+        self.layer.add(animation, forKey: "position")
     }
 }
 
